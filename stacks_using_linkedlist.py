ace1e78d37fb2ee988bce2f03088119f2629f635
ali-jee-786 <786syedalihussain@gmail.com>
2024-10-24 15:02:39 +0500

    updated

---
+++ stacks_using_linkedlist.py
@@ -0,0 +1,16 @@
+#Stacks using LinkedList:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def is_empty(self):
+        return self.top==None
+    
+    def push(self, data):
+        
